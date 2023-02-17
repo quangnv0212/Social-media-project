8bbdd0bab9e3b3ae6db028666acf72d4d3f00e58
Vu Quang <quangnv.0212@gmail.com>
2023-02-17 12:59:49 +0700

    Update Login function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -57,9 +57,11 @@ const register = catchAsync(async (req, res) => {
   });
   createSendToken(newUser, 201, res);
 });
-const login = async (req, res) => {
+const login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
-  console.log(req.body);
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password"), 400);
+  }
   // b1 : tìm ra user đang đăng nhập dựa trên trên email
   const user = await db.User.findOne({
     where: {
@@ -70,60 +72,12 @@ const login = async (req, res) => {
   if (user) {
     // b2 : kiểm mật khẩu có đúng hay không
     const isAuth = bcrypt.compareSync(password, user.password);
-    console.log("isAuth : ", isAuth);
     if (isAuth) {
-      // const token = jwt.sign(
-      //   { email: user.email, type: user.type },
-      //   "tuong-tinh-2350",
-      //   { expiresIn: 60 * 60 }
-      // );
-      // res.status(200).send({ message: "Đăng Nhập Thành Công ! ", token });
       createSendToken(user, 201, res);
     } else {
-      res.status(500).send({ message: "Tài khoãng hoặc mật khẩu không đúng" });
+      return next(new AppError("Incorrect email or password"), 401);
     }
-  } else {
-    res.status(404).send({ message: "Không tìm thấy email phù hợp" });
   }
-};
-
-const register2 = async (req, res) => {
-  const {
-    email,
-    password,
-    firstName,
-    lastName,
-    address,
-    gender,
-    roleId,
-    phonenumber,
-    positionId,
-    image,
-  } = req.body;
-  try {
-    // tạo ra một chuỗi ngẫu nhiên
-    const salt = bcrypt.genSaltSync(10);
-    // mã hóa salt + password
-    const hashPassword = bcrypt.hashSync(password, salt);
-    const newUser = await db.User.create({
-      email,
-      password: hashPassword,
-      firstName,
-      lastName,
-      address,
-      gender,
-      roleId,
-      phonenumber,
-      positionId,
-      image,
-    });
-    createSendToken(newUser, 201, res);
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      message: error,
-    });
-  }
-};
+});
 
 module.exports = { getAllUser, register, login };
